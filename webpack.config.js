94b550843940bf0f5daa7b24e202d69c935f933a
Zavid <zavidegbue@gmail.com>
2020-01-03 21:56:59 +0000

    Make it work

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,19 @@
-const webpack = require('webpack');
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-
-const isDevelopment = process.env.NODE_ENV === 'development'
-
 module.exports = {
   entry: './index.js',
   mode: 'none',
-  node: {
-    fs: 'empty',
-  },
   module: {
     rules: [
       {
-        test: /\.s(a|c)ss$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          }
-        ]
-      },
-      // {
-      //   test: /\.s(a|c)ss$/,
-      //   exclude: /\.module.(s(a|c)ss)$/,
-      //   loader: [
-      //     isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-      //     'css-loader',
-      //     {
-      //       loader: 'sass-loader',
-      //       options: {
-      //         sourceMap: isDevelopment
-      //       }
-      //     }
-      //   ]
-      // },
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
       }
     ],
   },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-    })
-  ],
-  resolve: {
-    extensions: ['.js', '.jsx', '.scss']
+  node: {
+    fs: 'empty',
   }
 };
