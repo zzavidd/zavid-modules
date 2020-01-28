f43d57c90af4f71e113932e34df9f0e630997f36
David Egbue <david.egbue@ibm.com>
2020-01-28 16:48:42 +0000

    Add more tests

--- test/constant_date_test.js
+++ test/constant_date_test.js
@@ -1,31 +1,53 @@
 const assert = require('chai').assert;
-const date = require('../constants/date.js');
+const zDate = require('../constants/date.js');
+const zRequest = require('../constants/request.js');
+
+zRequest();
 
 describe('Date functions', () => {
+    const datetime = '1996-12-02T08:39:39';
+
     it('Format full date', done => {
-        const dt = '1996-12-02';
-        const fullDate = date.formatDate(dt);
-        assert.equal(date.formatDate(dt), '2nd December 1996');
+        const fullDate = zDate.formatDate(datetime);
         assert.equal(fullDate, '2nd December 1996');
         done();
     });
 
     it('Format full date with day of week', done => {
-        const dt = '1996-12-02';
-        const fullDate = date.formatDate(dt, true);
+        const fullDate = zDate.formatDate(datetime, true);
         assert.equal(fullDate, 'Monday 2nd December 1996');
         done();
     });
+
+    it('Format to time', done => {
+        const formattedTime = zDate.formatTime(datetime);
+        assert.equal(formattedTime, '08:39');
+        done();
+    });
+
+    it('Format to full date and time', done => {
+        const fullDateTime = zDate.formatDateTime(datetime);
+        assert.equal(fullDateTime, '08:39 @ 2nd December 1996');
+        done();
+    });
+
+    // it('Format to ISO date', done => {
+    //     const datetime2 = 'Monday 2nd December 1996';
+    //     const isoDate = zDate.formatISODate(datetime2);
+    //     assert.equal(isoDate, '1996-12-02');
+    //     done();
+    // });
     
     it('Calculate age from birthday', done => {
         const birthday = '1996-12-02';
-        const age = date.calculateAge(birthday);
+        const age = zDate.calculateAge(birthday);
+        assert.equal(age, 23);
+        
+        // TODO: Make dynamic
+        // const dt = new Date(birthday);
+        // const now = dt.getTime();
+        // assert.equal(age, now / (365 * 24 * 3600000))
 
-        const dt = new Date(birthday);
-        const now = dt.getTime();
-
-        assert.equal(age, 23); // TODO: Make dynamic
-        // assert.equal(age, now / (365 * 24 * 3600000));
         done();
     });
 });
