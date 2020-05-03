5fc1df41093b4c675ba4800fe76bee635835dfb9
Zavid <zavidegbue@gmail.com>
2020-05-03 15:58:35 +0100

    Add checkbase64 function

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const form = require('./components/form');
 
 const countries = require('./constants/countries');
 const date = require('./constants/date');
+const file = require('./constants/file');
 const handlers = require('./constants/handlers');
 const request = require('./constants/request');
 const string = require('./constants/string');
@@ -11,6 +12,7 @@ module.exports = {
   
   zCountries: countries,
   zDate: date,
+  zFile: file,
   zHandlers: handlers,
   zRequest: request,
   zString: string
