c64709d7e883eef8b69730b0079d7760411527ba
Zavid <zavidegbue@gmail.com>
2020-05-11 20:44:31 +0100

    Complete zText

--- src/constants/text.js
+++ src/constants/text.js
@@ -4,26 +4,36 @@ const React = require('react');
 const EMPHASIS = {
   BOLD: 'bold',
   ITALIC: 'italic',
+  BOLDITALIC: 'boldItalic',
   UNDERLINE: 'underline',
   STRIKETHROUGH: 'strikethrough',
   HYPERLINK: 'hyperlink'
 };
 
+/** A map of section constants */
 const SECTIONS = {
-  TITLE: 'title',
-  SUBTITLE: 'subtitle',
-  IMAGE: 'image'
+  HEADING: 'heading',
+  SUBHEADING: 'subheading',
+  IMAGE: 'image',
+  DIVIDER: 'divider',
+  BULLET_LIST_ITEM: 'bullet',
+  HYPHEN_LIST_ITEM: 'hyphen',
+  BLOCKQUOTE: 'blockQuote'
 };
 
-/** The regex for an emphasis constant */
+/** The regex mapping for emphasis constants */
 const emphasisRegexMapping = {
+  [EMPHASIS.BOLDITALIC]: {
+    pure: new RegExp(/\*{3}(.*?)\*{3}/),
+    split: new RegExp(/(\*{3}.*?\*{3})/)
+  },
   [EMPHASIS.BOLD]: {
-    pure: new RegExp(/\*\*(.*?)\*\*/),
-    split: new RegExp(/(\*\*.*?\*\*)/)
+    pure: new RegExp(/\*{2}(.*?)\*{2}/),
+    split: new RegExp(/(\*{2}.*?\*{2})/)
   },
   [EMPHASIS.ITALIC]: {
-    pure: new RegExp(/\*(.*?)\*/),
-    split: new RegExp(/(\*.*?\*)/)
+    pure: new RegExp(/\*{1}(.*?)\*{1}/),
+    split: new RegExp(/(\*{1}.*?\*{1})/)
   },
   [EMPHASIS.UNDERLINE]: {
     pure: new RegExp(/\_(.*?)\_/),
@@ -39,29 +49,210 @@ const emphasisRegexMapping = {
   }
 };
 
+/** The regex mapping for section constants */
 const sectionRegexMapping = {
-  [SECTIONS.TITLE]: new RegExp(/^\#\s(.*?)$/),
-  [SECTIONS.SUBTITLE]: new RegExp(/^\#\#\s(.*?)$/),
-  [SECTIONS.IMAGE]: new RegExp(/^\!\[(.*?)\]\((.*?)\)$/)
+  [SECTIONS.HEADING]: new RegExp(/^\#\s(.*?)$/),
+  [SECTIONS.SUBHEADING]: new RegExp(/^\#{2}\s(.*?)$/),
+  [SECTIONS.IMAGE]: new RegExp(/^\!\[(.*?)\]\((.*?)\)$/),
+  [SECTIONS.DIVIDER]: new RegExp(/^(\-{3}|\_{3})$/),
+  [SECTIONS.BULLET_LIST_ITEM]: new RegExp(/^[\*\+]\s(.*?)$/),
+  [SECTIONS.HYPHEN_LIST_ITEM]: new RegExp(/^\-\s(.*?)$/),
+  [SECTIONS.BLOCKQUOTE]: new RegExp(/^\>\s(.*?)$/)
 };
 
-const emphasisRegexList = Object.values(emphasisRegexMapping).map(
-  (regex) => regex.split.source
-);
-const combinedRegex = new RegExp(emphasisRegexList.join('|'), 'g');
-
 /**
- * Apply markdown-like formatting to text.
- * @param {string} fullText - The text to which formatting needs to be applied.
- * @param {Object} hyperlinkClass - The CSS class for hyperlinks.
- * @returns The formatted text.
+ * Apply markdown-like formatting to a piece of text.
+ * @param {string} fullText - The text to which hierarchical formatting will be applied.
+ * @param {Object} hyperlinkClass - The CSS class to be passed into the next function.
+ * @returns {React.Component} The text with formatting applied.
  */
-const applyEmphasisFormatting = (fullText, hyperlinkClass) => {
+exports.formatText = (fullText, hyperlinkClass) => {
   if (!fullText) return '';
 
-  const fragments = fullText.split(combinedRegex).filter((e) => e != null);
-  const finalText = fragments.map((fragment, key) => {
-    let transformation;
+  const formattedText = fullText.split('\n').map((paragraph, key) => {
+    if (!paragraph) return null;
+
+    let transformedParagraph;
+
+    const foundSection = Object.entries(sectionRegexMapping).find(([, regex]) =>
+      regex.test(paragraph)
+    );
+
+    if (foundSection) {
+      const [section, regex] = foundSection;
+      const [, text] = paragraph.match(regex);
+
+      switch (section) {
+        case SECTIONS.HEADING:
+          transformedParagraph = <h1 key={key}>{text}</h1>;
+          break;
+        case SECTIONS.SUBHEADING:
+          transformedParagraph = <h2 key={key}>{text}</h2>;
+          break;
+        case SECTIONS.IMAGE:
+          const source = paragraph.match(regex)[2];
+          transformedParagraph = (
+            <div key={key}>
+              <img src={source} alt={text} style={STYLES.SECTIONS.IMAGE} />
+            </div>
+          );
+          break;
+        case SECTIONS.DIVIDER:
+          // TODO: Requires styling or classes
+          transformedParagraph = <hr style={STYLES.SECTIONS.DIVIDER} />;
+          break;
+        case SECTIONS.BULLET_LIST_ITEM:
+          transformedParagraph = (
+            <div key={key} style={STYLES.SECTIONS.LIST_ITEM}>
+              <span>●</span>
+              <span>{text}</span>
+            </div>
+          );
+          break;
+        case SECTIONS.HYPHEN_LIST_ITEM:
+          transformedParagraph = (
+            <div key={key} style={STYLES.SECTIONS.LIST_ITEM}>
+              <span>-</span>
+              <span>{text}</span>
+            </div>
+          );
+          break;
+        case SECTIONS.BLOCKQUOTE:
+          transformedParagraph = (
+            <div key={key} style={STYLES.SECTIONS.BLOCKQUOTE}>
+              {applyEmphasisFormatting(text, hyperlinkClass)}
+            </div>
+          );
+          break;
+        default:
+          break;
+      }
+    } else {
+      transformedParagraph = (
+        <p key={key}>{applyEmphasisFormatting(paragraph, hyperlinkClass)}</p>
+      );
+    }
+
+    return transformedParagraph;
+  });
+
+  return formattedText;
+};
+
+/**
+ * // TODO: Prevent hyperlink link remaining present in deformatting
+ * Strip the formatting from a piece of text.
+ * @param {string} fullText - The original text with formatting.
+ * @returns {string} The new text void of all formatting.
+ */
+exports.deformatText = (fullText) => {
+  if (!fullText) return '';
+
+  const deformattedText = fullText
+    .split('\n')
+    .map((paragraph, index) => {
+      if (!paragraph) return null;
+
+      // Initialise with space separate paragraphs
+      let detransformedParagraph = index > 0 ? ' ' : '';
+
+      const foundSection = Object.entries(
+        sectionRegexMapping
+      ).find(([, regex]) => regex.test(paragraph));
+
+      if (foundSection) {
+        const [section, regex] = foundSection;
+        const [, text] = paragraph.match(regex);
+
+        switch (section) {
+          case SECTIONS.HEADING:
+          case SECTIONS.SUBHEADING:
+          case SECTIONS.BULLET_LIST_ITEM:
+          case SECTIONS.HYPHEN_LIST_ITEM:
+            detransformedParagraph += text;
+            break;
+          case SECTIONS.IMAGE:
+          case SECTIONS.DIVIDER:
+            detransformedParagraph = null;
+            break;
+          case SECTIONS.BLOCKQUOTE:
+            detransformedParagraph += removeEmphasisFormatting(text);
+            break;
+          default:
+            break;
+        }
+      } else {
+        detransformedParagraph += removeEmphasisFormatting(paragraph);
+      }
+
+      return detransformedParagraph;
+    })
+    .join(' ');
+
+  return deformattedText;
+};
+
+/**
+ * Apply the variable substitutions to the text, swapping placeholders for dynamic values.
+ * @param {string} text - The original text containing the variables to be substituted.
+ * @param {Object} substitutions - The mapping specifying the values to substitute the placeholder variables.
+ * @returns {string} The full text with variables substitutions applied.
+ */
+exports.applySubstitutions = (text, substitutions) => {
+  if (text) {
+    const variableRegex = new RegExp(/\$\{(.*?)\}/g); // Regex for substitutions
+    text = text.replace(variableRegex, (match, p1) => substitutions[p1]);
+  }
+  return text;
+};
+
+/**
+ * Truncate a piece of text to a certain number of words.
+ * @param {string} originalText - The text to be truncated.
+ * @param {int} [limit] - The number of words to be truncated to. Default value is 45.
+ * @returns {string} The truncated text.
+ */
+exports.truncateText = (originalText, limit = 45) => {
+  if (!originalText) return '';
+  const deformattedText = this.deformatText(originalText);
+  const truncatedText = deformattedText.split(' ').slice(0, limit).join(' ');
+  if (truncatedText.length <= limit) return originalText;
+  return `${truncatedText}....`;
+};
+
+/**
+ * Extract an excerpt, which is a single deformatted paragraph, of a piece of text.
+ * @param {string} text - The original text.
+ * @returns {string} The excerpt shown in previews.
+ */
+exports.extractExcerpt = (originalText) => {
+  if (!originalText) return '';
+  const deformattedText = this.deformatText(originalText);
+  const excerpt = deformattedText.split('\n').filter((e) => e != null)[0];
+  return excerpt;
+};
+
+/**
+ * Apply emphasis formatting to paragraph of text.
+ * @param {string} paragraph - The text to which formatting needs to be applied.
+ * @param {Object} hyperlinkClass - The CSS class for hyperlinks.
+ * @returns {React.Component} The formatted text as a component.
+ */
+const applyEmphasisFormatting = (paragraph, hyperlinkClass) => {
+  if (!paragraph) return '';
+
+  // Combine all emphasis regular expressions for splitting
+  const emphasisRegexList = Object.values(emphasisRegexMapping).map(
+    (regex) => regex.split.source
+  );
+  const combinedEmphasisRegex = new RegExp(emphasisRegexList.join('|'), 'g');
+
+  // Split by regex and replace with formatted values
+  const fragments = paragraph
+    .split(combinedEmphasisRegex)
+    .filter((e) => e != null);
+  const formattedParagraph = fragments.map((fragment, key) => {
+    let transformation = fragment;
 
     const foundEmphasis = Object.entries(
       emphasisRegexMapping
@@ -72,6 +263,13 @@ const applyEmphasisFormatting = (fullText, hyperlinkClass) => {
       const [, text] = fragment.match(regex);
 
       switch (emphasis) {
+        case EMPHASIS.BOLDITALIC:
+          transformation = (
+            <strong key={key}>
+              <em>{text}</em>
+            </strong>
+          );
+          break;
         case EMPHASIS.BOLD:
           transformation = <strong key={key}>{text}</strong>;
           break;
@@ -80,7 +278,7 @@ const applyEmphasisFormatting = (fullText, hyperlinkClass) => {
           break;
         case EMPHASIS.UNDERLINE:
           transformation = (
-            <a key={key} style={{ textDecoration: 'underline' }}>
+            <a key={key} style={STYLES.EMPHASIS.UNDERLINE}>
               {text}
             </a>
           );
@@ -104,107 +302,52 @@ const applyEmphasisFormatting = (fullText, hyperlinkClass) => {
         default:
           break;
       }
-    } else {
-      transformation = fragment;
     }
 
     return transformation;
   });
 
-  return finalText;
+  return formattedParagraph;
 };
 
-/**
- * Apply the prefix formatting for hierarchical or listed text.
- * Text needs this formatting first before markdown formatting is applied.
- * @param {string} text - The text to which hierarchical formatting will be applied.
- * @param {Object} hyperlinkClass - The CSS class to be passed into the next function.
- * @returns The text with formatting applied.
- */
-exports.prefixFormatting = (text, hyperlinkClass) => {
-  if (text === null) return '';
+const removeEmphasisFormatting = (paragraph) => {
+  if (!paragraph) return '';
 
-  const finalText = text.split('\n').map((paragraph, key) => {
-    if (!paragraph) return null;
+  // Combine all emphasis regular expressions for splitting
+  const emphasisRegexList = Object.values(emphasisRegexMapping).map(
+    (regex) => regex.pure.source
+  );
+  const combinedEmphasisRegex = new RegExp(emphasisRegexList.join('|'), 'g');
 
-    let transformedParagraph;
+  // Split by regex and replace with deformatted values
+  const fragments = paragraph
+    .split(combinedEmphasisRegex)
+    .filter((e) => e != null);
 
-    const foundSection = Object.entries(sectionRegexMapping).find(([, regex]) =>
-      regex.test(paragraph)
-    );
+  const deformattedParagraph = fragments.map((fragment) => fragment).join(' ');
+  return deformattedParagraph;
+};
 
-    if (foundSection) {
-      const [section, regex] = foundSection;
-      const [, text] = paragraph.match(regex);
-
-      switch (section) {
-        case SECTIONS.TITLE:
-          transformedParagraph = <h1 key={key}>{text}</h1>;
-          break;
-        case SECTIONS.SUBTITLE:
-          transformedParagraph = <h2 key={key}>{text}</h2>;
-          break;
-        case SECTIONS.IMAGE:
-          const imgSrc = paragraph.match(regex)[2];
-          console.log(text, imgSrc);
-          transformedParagraph = (
-            <div key={key}>
-              <img src={imgSrc} alt={text} style={{ width: '100%' }} />
-            </div>
-          );
-          break;
-        default:
-          break;
-      }
-    } else {
-      transformedParagraph = (
-        <p key={key}>{applyEmphasisFormatting(paragraph, hyperlinkClass)}</p>
-      );
-    }
-
-    // switch (paragraph.charAt(0)) {
-
-    //   // For images
-    //   case ';':
-    //     const isFullImage = paragraph.charAt(1) === ';';
-    //     const imageType = isFullImage ? 'fullImage' : 'floatImage';
-    //     const offset = isFullImage ? 2 : 1;
-
-    //     return (
-    //       <div className={css[imageType]} key={key}>
-    //         <img
-    //           src={`${cloudinary.url}/public/fillers/${paragraph.substring(
-    //             offset
-    //           )}`}
-    //         />
-    //       </div>
-    //     );
-
-    //   // For dividers
-    //   case '_':
-    //     return <Divider key={key} style={{ margin: '2rem 0 1rem' }} />;
-
-    //   // For list items
-    //   case '•':
-    //     return (
-    //       <div className={css.listitem} key={key}>
-    //         <span>●</span>
-    //         <span>{this.applyEmphasisFormatting(paragraph.substring(1).trim())}</span>
-    //       </div>
-    //     );
-
-    //   // For normal paragraph text
-    //   default:
-    //     const finalText = this.applyEmphasisFormatting(paragraph, hyperlinkClass);
-    //     return (
-    //       <p className={css.body} key={key}>
-    //         {finalText}
-    //       </p>
-    //     );
-    // }
-
-    return transformedParagraph;
-  });
-
-  return finalText;
+const STYLES = {
+  SECTIONS: {
+    BLOCKQUOTE: {
+      borderLeft: '3px solid #dadada',
+      color: '#dadada',
+      paddingLeft: '1em',
+      margin: '1em 0'
+    },
+    DIVIDER: {
+      borderStyle: 'solid',
+      borderWidth: '1px',
+      margin: '2rem 0 1rem'
+    },
+    LIST_ITEM: {
+      display: 'grid',
+      gridTemplateColumns: '1.2em 1fr'
+    },
+    IMAGE: { width: '100%' }
+  },
+  EMPHASIS: {
+    UNDERLINE: { textDecoration: 'underline' }
+  }
 };
