a8e2778d523f449fc144b2e679323678241d348f
Zavid <zavidegbue@gmail.com>
2020-08-22 21:00:47 +0100

    Patch falsy verifier

--- src/constants/logic.js
+++ src/constants/logic.js
@@ -1,16 +1,17 @@
 /**
  * Checks if a value is falsy.
- * @param {any} value The value to check.
+ * @param {any} input The value to check.
  * @returns {boolean} True if falsy.
  */
-exports.isFalsy = (value) => {
-  if (value === undefined) return true;
-  if (value === null) return true;
-  if (value === false) return true;
-  if (value === '') return true;
-  if (value === 0) return true;
-  if (isNaN(value)) return true;
-
-  if (value.length === 0) return true;
+exports.isFalsy = (input) => {
+  if (input === undefined) return true;
+  if (input === null) return true;
+  if (input === false) return true;
+  if (input === '') return true;
+  if (input === 0) return true;
+  if (input.length === 0) return true;
+  if (typeof input === 'object'){
+    if (Object.values(input).length === 0) return true;
+  } 
   return false;
 };
--- test/logic.test.js
+++ test/logic.test.js
@@ -2,17 +2,49 @@ const assert = require('chai').assert;
 const zLogic = require('../src/constants/logic.js');
 
 describe('Logic tests', function () {
-  it('Check value is falsy', function () {
-    let emptyValue;
-    assert.isTrue(zLogic.isFalsy(emptyValue));
+  describe('Test falsy verification', function () {
+    describe('Check falsy values return true', function () {
+      it('Test undefined', function () {
+        let falsyValue;
+        assert.isTrue(zLogic.isFalsy(falsyValue));
+      });
 
-    emptyValue = null;
-    assert.isTrue(zLogic.isFalsy(emptyValue));
+      it('Test null', function () {
+        const falsyValue = null;
+        assert.isTrue(zLogic.isFalsy(falsyValue));
+      });
 
-    emptyValue = '';
-    assert.isTrue(zLogic.isFalsy(emptyValue));
+      it('Test empty string', function () {
+        const falsyValue = '';
+        assert.isTrue(zLogic.isFalsy(falsyValue));
+      });
 
-    emptyValue = 12 / 'twelve';
-    assert.isTrue(zLogic.isFalsy(emptyValue));
+      it('Test empty array', function () {
+        const falsyValue = [];
+        assert.isTrue(zLogic.isFalsy(falsyValue));
+      });
+
+      it('Test empty object', function () {
+        const falsyValue = {};
+        assert.isTrue(zLogic.isFalsy(falsyValue));
+      });
+    });
+
+    describe('Check truthy values return false', function () {
+      it('Test occupied string', function () {
+        const truthyValue = 'truthy';
+        assert.isFalse(zLogic.isFalsy(truthyValue));
+      });
+
+      it('Test occupied array', function () {
+        const truthyValue = ['truthy'];
+        assert.isFalse(zLogic.isFalsy(truthyValue));
+      });
+
+      it('Test occupied object', function () {
+        const truthyValue = { truthy: true };
+        assert.isFalse(zLogic.isFalsy(truthyValue));
+      });
+    });
   });
 });
